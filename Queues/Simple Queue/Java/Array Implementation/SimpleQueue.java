627e9988d54b0f3a2b2aa8afe39bb94e8298b002
Jose Nunez <43043747+ThatDudeAlex@users.noreply.github.com>
2022-04-25 17:11:35 -0400

    implemented a queue using an array

---
+++ Queues/Simple Queue/Java/Array Implementation/SimpleQueue.java
@@ -0,0 +1,116 @@
+/**
+ * An implementation of a standard Doubly Linked List.
+ * 
+ * @author Alex Nunez 
+ */
+public class SimpleQueue {    
+    private int[]   queue;
+    private int     front;
+    private int     back;
+    private int     size;
+
+    /**
+     * Initializes an empty queue
+     */
+    SimpleQueue(int capacity) {
+        this.queue = new int[capacity];
+        this.front = 0;
+        this.back  = 0;
+        this.size  = 0;
+    }
+
+    /**
+     * Initializes a queue with 1 {@code element}
+     * 
+     * @param data the value of the first {@code element} in the queue
+     */
+    SimpleQueue(int capacity, int data) {
+        this.queue = new int[capacity];
+        this.queue[0] = data;
+        this.front    = 0;
+        this.back     = 1 % capacity;
+        this.size     = 1;
+    }
+
+    /**
+     * Inserts a new value at the end of the queue
+     * 
+     * @param data The value that the new node will hold
+     * 
+     * @Runtime {@code O(1)} - because its not affected by the size of the queue, since it just 
+     * directly updates a single index in the queue array
+     */
+    public boolean enqueue(int data) {
+        if (isFull())
+            return false;
+        
+        this.queue[this.back] = data;
+        this.back = (this.back + 1) % this.queue.length;
+        this.size++;
+
+        return true;
+    }
+
+    /**
+     * Removes the head of the queue and returns it. If the queue is empty, it returns null
+     * 
+     * @return The first {@code element} in the list
+     * 
+     * @Runtime {@code O(1)} - because its not affected by the size of the queue, since it just adds +1 
+     * to the value of the front index
+     */
+    public Integer dequeue() {
+        if (isEmpty())
+            return null;
+
+        int head = this.queue[this.front];
+        this.front = (this.front + 1) % this.queue.length;
+        this.size--;
+
+        return head;
+    }
+
+    /**
+     * Returns the head (front) of the queue without removing it.
+     * 
+     * @return The first {@code element} in the queue if it exits, else returns null
+     * 
+     * @Runtime {@code O(1)} - because its not affected by the size of the queue
+     */
+    public Integer peek() {
+        return (isEmpty()) ? null : this.queue[this.front];
+    }
+
+    /**
+     * Returns the current size of the queue
+     * 
+     * @return the number of {@code elements} currently in the queue
+     * 
+     * @Runtime {@code O(1)}
+     */
+    public int size() {
+        return this.size;
+    }
+
+    /**
+     * Verifies if the queue is currently empty
+     * 
+     * @return {@code true} if the queue contains no {@code Values}, else returns {@code false}
+     * 
+     * @Runtime {@code O(1)} - because it just does a simple boolean comparison
+     */
+    public boolean isEmpty() {
+        return this.size == 0 && front == back;
+    }
+
+    /**
+     * Verifies if the queue is currently full
+     * 
+     * @return {@code true} if the queue no open slots, else returns {@code false}
+     * 
+     * @Runtime {@code O(1)} - because it just does a simple boolean comparison
+     */
+    public boolean isFull() {
+        return this.size == this.queue.length - 1;
+    }
+}
---
+++ Queues/Simple Queue/Java/Array Implementation/SimpleQueueTest.java
@@ -0,0 +1,167 @@
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+
+public class SimpleQueueTest {
+    public static void main(String[] args) {
+        int testNumber = 1;
+
+        // Iterates through all the public methods in the class and invokes each one except main().
+        // This makes calling & adding additional testcases easier
+        
+        for (Method method : SimpleQueueTest.class.getDeclaredMethods()) {
+            if (method.getName() != "main" && Modifier.isPublic(method.getModifiers())) {
+                try {
+                    method.invoke(method, new Object[] { method.getName(), testNumber++ });
+                } catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+                    testcaseError(e, testNumber++);
+                }
+            }
+        }
+    }
+
+        /*
+     * ======================
+     * List Init test
+     * ======================
+     */
+
+    public static void testInitListWithoutParams(String funcName, int testNumber) {
+        SimpleQueue queue = new SimpleQueue(10);
+        Integer front = queue.peek();
+
+        if (queue.size() != 0 || front != null)
+            failTest(funcName, testNumber);
+        else
+            passTest(funcName, testNumber);
+    }
+
+    public static void testInitListWithParams(String funcName, int testNumber) {
+        SimpleQueue queue = new SimpleQueue(10, 4);
+        Integer front = queue.peek();
+
+        if (queue.size() != 1 || front == null || front != 4)
+            failTest(funcName, testNumber);
+        else
+            passTest(funcName, testNumber);
+    }
+
+    /*
+     * ======================
+     * enqueue() test
+     * ======================
+     */
+
+    public static void testEnqueueOnEmptyQueue(String funcName, int testNumber) {
+        SimpleQueue queue = new SimpleQueue(10);
+
+        queue.enqueue(2);
+        Integer front = queue.peek();
+
+        if (queue.size() != 1 || front == null || front != 2)
+            failTest(funcName, testNumber);
+        else
+            passTest(funcName, testNumber);
+    }
+
+    public static void testEnqueueOnNonEmptyQueue(String funcName, int testNumber) {
+        SimpleQueue queue = new SimpleQueue(10, 2);
+
+        queue.enqueue(3);
+        Integer front = queue.peek();
+
+        if (queue.size() != 2 || front == null || front != 2)
+            failTest(funcName, testNumber);
+        else
+            passTest(funcName, testNumber);
+    }
+
+    /*
+     * ======================
+     * dequeue() test
+     * ======================
+     */
+
+    public static void testDequeueOnEmptyQueue(String funcName, int testNumber) {
+        SimpleQueue queue = new SimpleQueue(10);
+        Integer front = queue.dequeue();
+
+        if (queue.size() != 0 || front != null)
+            failTest(funcName, testNumber);
+        else
+            passTest(funcName, testNumber);
+    }
+
+    public static void testDequeueOnNonEmptyQueue(String funcName, int testNumber) {
+        SimpleQueue queue = new SimpleQueue(10, 2);
+        queue.enqueue(3);
+
+        Integer front1 = queue.dequeue();
+        Integer front2 = queue.dequeue();
+
+        if (queue.size() != 0 || front1 != 2 || front2 != 3)
+            failTest(funcName, testNumber);
+        else
+            passTest(funcName, testNumber);
+    }
+
+    /*
+     * ======================
+     * isEmpty() test
+     * ======================
+     */
+
+    public static void testIsEmptyOnEmptyQueue(String funcName, int testNumber) {
+        SimpleQueue queue = new SimpleQueue(10);
+
+        if (queue.isEmpty())
+            passTest(funcName, testNumber);
+        else
+            failTest(funcName, testNumber);
+    }
+
+    public static void testIsEmptyOnNonEmptyQueue(String funcName, int testNumber) {
+        SimpleQueue queue = new SimpleQueue(10, 2);
+        queue.enqueue(3);
+
+        if (queue.isEmpty())
+            failTest(funcName, testNumber);
+        else
+            passTest(funcName, testNumber);
+    }
+
+    /*
+     * ======================
+     * Pointers test
+     * ======================
+     */
+     
+    public static void testFrontAndBackPointerWrapAroundQueue(String funcName, int testNumber) {
+        SimpleQueue queue = new SimpleQueue(3, 1);
+        queue.enqueue(2); queue.enqueue(3);
+
+        if (!queue.isFull()) 
+            failTest(funcName, testNumber);
+        else if (queue.dequeue() != 1 && !queue.enqueue(4) && queue.isFull())
+            failTest(funcName, testNumber);
+        else if (queue.dequeue() != 2 && queue.dequeue() != 3 && queue.dequeue() != 4 && !queue.isEmpty())
+            failTest(funcName, testNumber);
+        else 
+            passTest(funcName, testNumber);
+    }
+
+    private static void failTest(String funcName, int testNumber) {
+        String red = "\u001B[31m";
+        System.out.printf("\n%s%d - %s : FAIL\n", red, testNumber++, funcName);
+    }
+
+    private static void passTest(String funcName, int testNumber) {
+        String green = "\u001B[32m";
+        System.out.printf("\n%s%d - %s : PASS\n", green, testNumber++, funcName);
+    }
+
+    private static void testcaseError(Exception e, int testNumber) {
+        String yellow = "\u001b[33m";
+        System.out.printf("\n%s%d - Testcase Error: %s\n", yellow, testNumber++, e.getMessage());
+    }
+}
